be93244b66ec1ff356edea17912f23f7f1e7b1c5
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-12-12 12:07:30 +0100

    fichier script de creation de base de données

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// on definit les routes ici
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello, World!';
 });
