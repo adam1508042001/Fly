b8166a772f810f0b9ab4eddb10cba937dff5d6fc
AFROUNE MOHAMED ADAM <148912667+adam1508042001@users.noreply.github.com>
2024-12-14 12:56:11 +0100

    validation de formulaire done

--- front/vue/src/views/SignIn.vue
+++ front/vue/src/views/SignIn.vue
@@ -4,7 +4,7 @@
     <NavBar />
 
     <div class="flex items-center justify-center flex-grow bg-cover bg-center">
-      <div class="bg-black bg-opacity-65  p-8 rounded-[40px] shadow-lg w-[500px] h-[400px]">
+      <div class="bg-black bg-opacity-65  p-8 rounded-[40px] shadow-lg w-[600px] ">
 
         <!-- Titre -->
         <div class="text-start mb-6">
@@ -13,10 +13,12 @@
         </div>
 
         <!-- Formulaire -->
-        <form>
+         <!-- @submit.prevent pour ne pas   recharger  la page pour pouvoir gérer le formualire  -->
+        <form  @submit.prevent="validation" >
           <!-- Email -->
-          <div class="mb-4">
+          <div  class="mb-4">
             <label for="email" class="block  font-bold text-white">Email</label>
+            <!-- j'ajoute des variablmes reactives pour stocker les valeurs  avec id -->
             <input
               type="email"
               id="email"
@@ -31,12 +33,23 @@
             <label for="password" class="block font-bold text-white">Password</label>
             <input
               type="password"
+
               id="password"
               v-model="password"
               placeholder="user password"
               class="mt-1 block w-full px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500 shadow-sm"
             />
           </div>
+
+          <!-- Affichage des erreurs -->
+          <div v-if="errors.length" class="mb-4 text-red-500  ">
+            <ul>
+              <!-- vfor est faite pour itérer sur l'objet errors et afficher des élement en fonctions de ces données  -->
+              <!-- item : un élément dans la liste; :key : Une clé unique (souvent un id ou l'index) qui aide Vue à suivre les éléments lors des mises à jour -->
+              <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+            </ul>
+          </div>
+
 
           <!-- Bouton de connexion -->
           <div class="mb-4">
@@ -60,7 +73,44 @@
 </template>
 
 <script setup>
-import NavBar  from './NavBar.vue';
+import { ref } from 'vue';
+import NavBar  from '../views/NavBar.vue';
+
+  
+  // Variables réactives pour les champs
+  
+  const email = ref('');
+  const password = ref('');
+  const errors = ref([]);
+
+
+
+
+
+    // Méthode pour la validation du formulaire
+    const validation = (e)  => {
+
+      errors.value = []; 
+
+
+      if (email.value && password.value) {
+        alert ('formulaire validé ! '); 
+        return true;
+      }
+      
+
+
+      if (!email.value) {
+        errors.value.push('Email must not be empty');
+      }
+      if (!password.value) {
+        errors.value.push('please enter a password');
+      }
+
+    
+    }
+  
+
 </script>
 
 <style>
